6a6448f33512fa7a951fe14b84f3b836a968e043
ryuna <anottrx@gmail.com>
2022-04-25 20:24:28 +0900

    Chore : .env 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    REACT_APP_LOCALHOST_URL: process.env.REACT_APP_LOCALHOST_URL,
+    REACT_APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
+    REACT_APP_GOOGLE_KEY: process.env.REACT_APP_GOOGLE_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
