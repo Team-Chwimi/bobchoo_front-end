c970a0e34ec94e7e64504eef1b86656db4ea1756
ryuna <anottrx@gmail.com>
2022-05-02 14:44:14 +0900

    Design : 메인페이지 마크업

--- data/palette.ts
+++ data/palette.ts
@@ -1,6 +1,8 @@
 type PaletteColor =
+  | 'white'
   | 'orange'
   | 'orange_point'
+  | 'gray_52'
   | 'gray_F2'
   | 'gray_79'
   | 'gray_38';
@@ -8,9 +10,11 @@ type PaletteColor =
 type PaletteType = Record<PaletteColor, string>;
 
 export const PALETTE: PaletteType = {
+  white: '#fff',
   orange: '#FAAC69',
   orange_point: '#FF7B30',
   gray_F2: '#F2F2F2',
+  gray_52: '#525252',
   gray_79: '#797979',
   gray_38: '#383838',
 };
