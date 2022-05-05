ac3270fc9328847de5d21e0b16dd3423a07f90a4
ryuna <anottrx@gmail.com>
2022-05-05 23:57:49 +0900

    Feat : 현재 위치 및 지도 중앙에 마커 구현 (#8)

--- components/map/mapSection.tsx
+++ components/map/mapSection.tsx
@@ -26,12 +26,34 @@ const MapSection: React.FC = () => {
       version: 'weekly',
     });
     let map: any;
-    // let marker: any;
+    let marker: any;
     loader.load().then(() => {
       map = new google.maps.Map(mapRef.current, {
         center: { lat: Number(latlng.lat), lng: Number(latlng.lng) },
         zoom: 16,
       });
+      marker = new window.google.maps.Marker({
+        position: {
+          lat: Number(latlng.lat) || 37.5666784,
+          lng: Number(latlng.lng) || 126.9778436,
+        },
+        map,
+      });
+      map.addListener(
+        'center_changed',
+        throttle(() => {
+          const centerLat = map.getCenter().lat();
+          const centerLng = map.getCenter().lng();
+          marker.setPosition({ lat: centerLat, lng: centerLng });
+          // dispatch(
+          //   latlngActions.setLatLng({
+          //     lat: centerLat.toString(),
+          //     lng: centerLng.toString(),
+          //     hasCurrentLoaction: true,
+          //   }),
+          // );
+        }, 150),
+      );
     });
   });
 
