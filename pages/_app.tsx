e3b93575c36c7eca98191fa7b695db7964e72ea6
ryuna <anottrx@gmail.com>
2022-05-12 11:05:26 +0900

    Modify : ReactQueryDevtools 적용 (#28)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,34 @@
+import React from 'react';
+
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
 import '../styles/reset.css';
 import '../styles/globals.css';
 
-import type { AppProps } from 'next/app';
-
 import { wrapper } from '../store';
 
-import Head from 'next/head';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const app = ({ Component, pageProps }: AppProps) => {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <Head>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="../images/bobdol_logo.png" />
-        <title>밥추</title>
-        <meta name="description" content="당신을 위한 식사 메뉴 추천 서비스" />
-      </Head>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="../images/bobdol_logo.png" />
+          <title>밥추</title>
+          <meta
+            name="description"
+            content="당신을 위한 식사 메뉴 추천 서비스"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 };
