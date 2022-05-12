1328c9c458d82e60385b1043048da6006f134982
eylee1113 <eylee1113@naver.com>
2022-05-13 03:33:46 +0900

    Design : 설문조사 질문부분  스타일링 (#31)

--- components/common/survey/question.tsx
+++ components/common/survey/question.tsx
@@ -10,6 +10,7 @@ interface StoreDetailProps{
 const Question: React.FC<StoreDetailProps> = ({id})=>{
   
   const data = useSelector((state) => state.question.questions);
+  const qestionTotal = useSelector((state) => state.question.questionTotalCount);
   const dataDetail = data[id-1];
   useEffect(() => {
     console.log(dataDetail);
@@ -21,24 +22,81 @@ const Question: React.FC<StoreDetailProps> = ({id})=>{
       <Wrapper>
         <QuestionDiv>
           <Title>{dataDetail.question}</Title>
-          <ImageWrapper src="/images/bobdol.gif" alt="밥돌이 이미지" />
+            {(dataDetail.answerList.length>2) && <OverlapDiv>(중복 선택 가능)</OverlapDiv>}
         </QuestionDiv>
+        <ImageWrapper src="/images/bobdol.gif" alt="밥돌이 이미지" />
+        <GraphDiv>
+          <GraphBack>
+            <Graph goal={qestionTotal} total={id}></Graph>
+          </GraphBack>
+          <SubDiv>{id}/{qestionTotal}</SubDiv>
+        </GraphDiv>
       </Wrapper>
     </Container>
   );
 };
+
 const Container = styled.div``;
 const Wrapper = styled.div``;
-const QuestionDiv = styled.div``;
+const QuestionDiv = styled.div`
+  width:60%;
+  float: left; 
+`;
 const Title = styled.div`
-  font-size: 20px;
+  font-size: 30px;
   font-style: normal;
   font-weight: 800;
-  margin-top:2vh;
+  margin-top:5vh;
+  margin-left:2vh;
+  word-break: keep-all;
+
+`;
+const OverlapDiv = styled.div`
+  clear:both;
+  margin-left:2vh;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  margin-top:1vh;
+  color: #797979;
+
 `;
 const ImageWrapper = styled.img`
   // width: 61px;
-  height: 61px;
+  height: 100px;
+  float: right; 
+  margin-top:3vh;
+  margin-right:2vh;
+`;
+
+const GraphDiv = styled.div`
+  clear:both;
+  padding: 2vh;
+`;
+const GraphBack = styled.div`
+  height: 10px;
+  background-color: #FAAC69;
+  border-radius: 10px;
+`;
+
+export const Graph = styled.div`
+  z-index=0;
+  position: relative;
+  // top: -10px;
+  height: 10px;
+  background-color: #FF7B30;
+  border-radius: 10px;
+  width: ${(props) => {
+    return (props.total / props.goal) * 100;
+  }}%;
+`;
+const SubDiv = styled.div`
+  float: right; 
+  font-style: normal;
+  font-weight: 800;
+  font-size: 20px;
+  color: #B9B9B9;
+  margin-top:1vh;
 `;
 
 export default Question;
