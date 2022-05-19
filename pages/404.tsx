ce9fcebd25aaae13cec4ec95c286e6935b80b8a5
ryuna <anottrx@gmail.com>
2022-05-20 03:20:11 +0900

    Design : 404 에러 페이지 스타일링 수정

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,44 @@
 import type { NextPage } from 'next';
 
-import ErrorSection from '../components/common/errorSection';
+import styled from '@emotion/styled';
+
+import Header from '../components/common/header';
+
+import { LINK_HOME } from '../data/link';
 
 const Custom404: NextPage = () => {
-  return <ErrorSection errorMessage="요청하신 페이지를 찾을 수 없습니다" />;
+  return (
+    <Container>
+      <Header linkName={LINK_HOME.linkName} linkPath={LINK_HOME.linkPath} />
+      <Wrapper>
+        <Img src="/images/404_error.gif" alt="404 에러 이미지" />
+      </Wrapper>
+    </Container>
+  );
 };
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 0 16px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 15vh;
+`;
+
+const Img = styled.img`
+  width: 60%;
+
+  @media (max-width: 991px) {
+    width: 85%;
+  }
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+`;
 
 export default Custom404;
